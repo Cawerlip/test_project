4d94b74164ddf6f470c9c4c24d3a3e904368f5d1
Sofia Polevaya <polevaya@sminex.com>
2024-01-08 21:22:41 +0300

    Module-1_7
    add theme context

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,18 @@
+import { Suspense } from "react"
 import { Route, Routes, Link } from "react-router-dom"
-import './index.scss'
+
 import { MainPageAsync } from "../pages/MainPage/MainPage.async"
 import { AboutPageAsync } from "../pages/AboutPage/AboutPage.async"
-import { Suspense } from "react"
+import { useTheme } from "../theme/useTheme"
+
+import '../styles/index.scss'
 
 export const App = () => {
+const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>toggle theme</button>
       <Link to={'/'}>Main Page</Link>
       <Link to={'/about'}>About Page</Link>
       <Suspense fallback={<div>Loading...</div>}>
---
+++ src/theme/ThemeContext.ts
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+
+export enum Theme {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
+
+export interface IThemeContextProps {
+  theme?: Theme
+  setTheme?: (theme: Theme) => void
+}
+
+export const LOCAL_STORAGE_THEME_KEY = 'theme'
+
+export const ThemeContext = createContext<IThemeContextProps>({})
