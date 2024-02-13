ff230fee125cfd53003d8f08d9dc38bcc1f0e597
Sofia Polevaya <polevaya@sminex.com>
2024-02-13 16:27:03 +0300

    fix

--- src/app/providers/ErrorBoundary/ui/BugButton.tsx
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 
 export const BugButton = () => {
   const [error, setError] = useState<boolean>(false)
@@ -14,6 +14,7 @@ export const BugButton = () => {
   }, [error])
 
   return (
+    // eslint-disable-next-line i18next/no-literal-string
     <button onClick={onThrowError} type="button">throw error</button>
   )
 }
